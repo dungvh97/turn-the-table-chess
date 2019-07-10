73419ef5ceb1634886338145ed453db5520cc701
dungvh97 <dungvh97@gmail.com>
2019-07-10 21:49:27 +0700

    c2: load image

---
+++ app/src/androidTest/java/vn/dn/freelancer/chess_turn_the_table/ExampleInstrumentedTest.kt
@@ -0,0 +1,26 @@
+package vn.dn.freelancer.chess_turn_the_table
+
+import android.content.Context
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("vn.dn.freelancer.chess_turn_the_table", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/vn/dn/freelancer/chess_turn_the_table/MainActivity.kt
@@ -0,0 +1,12 @@
+package vn.dn.freelancer.chess_turn_the_table
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
